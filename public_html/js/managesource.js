6842f6ac2fa4c466970be0893d8baf0c99e467ab
Avash Gautam <avashgtm@gmail.com>
2015-07-13 10:28:02 +0545

    bug fixes on site.js,managesource.js

--- public_html/js/managesource.js
+++ public_html/js/managesource.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (function($){
 	var panelCount = 1;
 	$("#newsource").click(function(e){
@@ -21,27 +20,3 @@
 		collapsable.find('input').eq(0).focus();
 	});
 })(jQuery);
-=======
-(function($){
-	var panelCount = 1;
-	$("#newsource").click(function(e){
-		e.preventDefault();
-		var panel = $("#newsourcepanel .panel");
-		panel.find('.panel-title a').attr('href',"#panel"+panelCount);
-		panel.find('.panel-collapse').attr('id',"panel"+panelCount);
-		panel.clone().prependTo("#accordion");
-		panelCount++;
-
-		if($('#accrodion').children().length == 0){
-			$('.savesourcesouter').removeClass('hide');
-		}
-	});
-	$(".editsource").click(function(){
-		var id = $(this).data('id'),
-			collapsable = $("#collapseOne"+id);
-
-		collapsable.collapse('show');
-		collapsable.find('input').eq(0).focus();
-	});
-})(jQuery);
->>>>>>> 22be403ff560b2bebfd1c48a952bcdee7ef708da
--- public_html/js/site.js
+++ public_html/js/site.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 	function openModal(route, title, body, modal){
 		modal = modal || $(".modal");
 		modal.find('.modal-title').html(title);
@@ -15,36 +14,9 @@
 		openModal(route, "Channel: delete","Are you sure you want to delete this channel?");
 	}
 	function confirmDelete(modal, route){
-alert(route);
 		if(route){
 			if(modal){
 				modal.modal('hide');
 			}
 			window.location.href = route;
 		}
-=======
-	function openModal(route, title, body, modal){
-		modal = modal || $(".modal");
-		modal.find('.modal-title').html(title);
-		modal.find('.modal-body').html(body);
-		modal.modal();
-
-		$(".confirmyes").click(function(){
-			confirmDelete(modal, route);
-		});
-	}
-	function openchanneldeletemodal(e, self){
-		e.preventDefault();
-		var route = self.attr('href');
-		openModal(route, "Channel: delete","Are you sure you want to delete this channel?");
-	}
-	function confirmDelete(modal, route){
-alert(route);
-		if(route){
-			if(modal){
-				modal.modal('hide');
-			}
-			window.location.href = route;
-		}
->>>>>>> 22be403ff560b2bebfd1c48a952bcdee7ef708da
-	}
