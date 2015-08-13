cfb48905d621e339eb4d0af311091817273918ff
Avash Gautam <avashgtm@gmail.com>
2015-08-13 16:25:31 +0545

    modified kernel.php for testing 15

--- laravel/app/Console/Kernel.php
+++ laravel/app/Console/Kernel.php
@@ -29,37 +29,46 @@ class Kernel extends ConsoleKernel {
 		->hourly();
 		$schedule->call(function(){
 			//sendNotification('test notification 1');
-										ParseClient::initialize(
-           'fPSUGZ0H5wm7UPgcEYQ3EImEgv3HuidGeFXFDDJw',
-           '6VIhRzVVQN8oBsYjbZ2SYCmBzEqK4C499o4Q25KD',
-           'c6akmuK1fHz8RcYuwn6bh5EhaXvqeeZdezc6xbpj'
-        );
-        $query = ParseInstallation::query();
-$query->equalTo('channels', '');
-ParsePush::send(array(
-  "where" => $query,
-  "data" => array(
-    "alert" => "test with new notification",
-    "typeId" => "0001",
-  )
-));
-        \Log::info(error_get_last ());
-		})->dailyAt('9:50');
+			ParseClient::initialize(
+				'ZBGNVDDcZJdilnPVkxTrXOe8U0ToWZcfWAqRTupN',
+				'bhtvPPoz8siyZO0RbS7sGYmvpusa8YubzxIi7Oa9',
+				'd269BnkUvTv83AvbqFnKoRVb9gHRmMQDZht4kpuc'
+				);
+			$query = ParseInstallation::query();
+			$query->equalTo('channels', '');
+			$types = ['live','latest','featured','popular','news'];
+			foreach($types as $typeId):
+			$not_data =[];
+				if($type=="news"){
+					$not_data = ['newsId'=>600];
+				}
+			ParsePush::send(array(
+				"where" => $query,
+				"data" => array(
+					"alert" => "test with new notification for tab ".$typeId,
+					"nitv_b_typeId" => $typeId,
+					"nitv_b_data"=>$not_data
+					)
+				));
+			endforeach;
+
+			\Log::info(error_get_last ());
+		})->dailyAt('10:42');
 	}
 
 	function sendNotification($msg){
-							ParseClient::initialize(
-           'fPSUGZ0H5wm7UPgcEYQ3EImEgv3HuidGeFXFDDJw',
-           '6VIhRzVVQN8oBsYjbZ2SYCmBzEqK4C499o4Q25KD',
-           'c6akmuK1fHz8RcYuwn6bh5EhaXvqeeZdezc6xbpj'
-        );					
+		ParseClient::initialize(
+			'fPSUGZ0H5wm7UPgcEYQ3EImEgv3HuidGeFXFDDJw',
+			'6VIhRzVVQN8oBsYjbZ2SYCmBzEqK4C499o4Q25KD',
+			'c6akmuK1fHz8RcYuwn6bh5EhaXvqeeZdezc6xbpj'
+			);					
 		ParsePush::send(
-            [
-            'channel'=>['broadcast'],
-            'data'     => ['alert' => $msg],
-            ]
-        );
-        \Log::info(error_get_last ());
+			[
+			'channel'=>['broadcast'],
+			'data'     => ['alert' => $msg],
+			]
+			);
+		\Log::info(error_get_last ());
 	}
 
 }
