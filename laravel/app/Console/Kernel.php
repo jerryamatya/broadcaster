150bcfa622707b705fde7a212b82708387bf8b77
Avash Gautam <avashgtm@gmail.com>
2015-08-13 13:25:05 +0545

    modified kernel.php for testing 4

--- laravel/app/Console/Kernel.php
+++ laravel/app/Console/Kernel.php
@@ -27,8 +27,20 @@ class Kernel extends ConsoleKernel {
 		$schedule->command('inspire')
 		->hourly();
 		$schedule->call(function(){
-			sendNotification('test notification 1');
-		})->dailyAt('7:36');
+			//sendNotification('test notification 1');
+										ParseClient::initialize(
+           'fPSUGZ0H5wm7UPgcEYQ3EImEgv3HuidGeFXFDDJw',
+           '6VIhRzVVQN8oBsYjbZ2SYCmBzEqK4C499o4Q25KD',
+           'c6akmuK1fHz8RcYuwn6bh5EhaXvqeeZdezc6xbpj'
+        );					
+		ParsePush::send(
+            [
+            'channel'=>['broadcast'],
+            'data'     => ['alert' => $msg],
+            ]
+        );
+        \Log::info(error_get_last ());
+		})->dailyAt('7:42');
 	}
 
 	function sendNotification($msg){
